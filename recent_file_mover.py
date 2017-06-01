5ee1e97e4eca82ade396df3c329e4817270787f9
reismahnic <reismahnic@gmail.com>
2017-05-31 19:45:40 -0700

    Initial Commit
    A program that moves files edited in the last 24 hours from one folder
    to another

---
+++ recent_file_mover.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+import datetime as dt
+
+#List the source folder and destination folder
+source = 'C:\Users\Reis\Desktop\Folder A'
+destination = 'C:\Users\Reis\Desktop\Folder B'
+
+#Define the current time and the time period we want to look back at
+now = dt.datetime.now()
+before = now - dt.timedelta(hours=24)
+
+#Print the list of file names
+files = os.listdir(source)
+
+for root,dirs,files in os.walk('C:\Users\Reis\Desktop\Folder A'):  
+    for file_name in files:
+        path = os.path.join(root,file_name)
+        st = os.stat(path)    
+        mod_time = dt.datetime.fromtimestamp(st.st_ctime)
+        if mod_time > before:
+            #Move all files in Folder A to Folder B
+            shutil.move(os.path.join(root, file_name), destination)
