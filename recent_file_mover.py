42d27ea72624b4cd2f66521f50b07f27ad5e1633
reismahnic <reismahnic@gmail.com>
2017-06-01 15:52:20 -0700

    Edit- new commit
    Previously program moved files based on when they were created- now they
    move based on when modified.

--- recent_file_mover.py
+++ recent_file_mover.py
@@ -17,7 +17,7 @@ for root,dirs,files in os.walk('C:\Users\Reis\Desktop\Folder A'):
     for file_name in files:
         path = os.path.join(root,file_name)
         st = os.stat(path)    
-        mod_time = dt.datetime.fromtimestamp(st.st_ctime)
+        mod_time = dt.datetime.fromtimestamp(st.st_mtime)
         if mod_time > before:
             #Move all files in Folder A to Folder B
             shutil.move(os.path.join(root, file_name), destination)
